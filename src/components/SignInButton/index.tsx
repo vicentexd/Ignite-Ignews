6a15642325c67d25e32f217d5256252955c2ad10
Gabriel Vicente <vicente.gabrielv@gmail.com>
2021-04-15 15:50:34 -0300

    Compentes Header e Button

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,27 @@
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+
+export function SignInButton() {
+  const isUserLoggedIn = false;
+
+  return isUserLoggedIn ? (
+    <button 
+    type='button'
+    className={styles.signInButton}
+    >
+      <FaGithub color="#04d361" />
+      Vicente
+      <FiX color="#737380" className={styles.rigthIcon}/>
+    </button>
+  ): (
+    <button 
+    type='button'
+    className={styles.signInButton}
+    >
+      <FaGithub color="#eda417" />
+      Sign in with Github
+    </button>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+     <Head>
+        <title>TEste</title>
+      </Head>
+      <h1>
+     
+      Olá
+      </h1>
+    </>
+    
+  )
+}
