704e9b85d08b27a1773446adfda40d34e34ac631
Gabriel Vicente <vicente.gabrielv@gmail.com>
2021-04-16 12:28:58 -0300

    Configurando redirecionamento do Stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,24 +1,29 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
+import { signIn, signout, useSession } from 'next-auth/client';
 
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  console.log('session', session);
+
+  return session ? (
     <button 
     type='button'
     className={styles.signInButton}
+    onClick={() => signout()}
     >
       <FaGithub color="#04d361" />
-      Vicente
+      {session.user.name}
       <FiX color="#737380" className={styles.rigthIcon}/>
     </button>
   ): (
     <button 
     type='button'
     className={styles.signInButton}
+    onClick={() => signIn('github')}
     >
       <FaGithub color="#eda417" />
       Sign in with Github
