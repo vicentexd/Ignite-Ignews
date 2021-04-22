fbbf11f56b954718f7dc70ac4ead4346ef70d961
Gabriel Vicente <vicente.gabrielv@gmail.com>
2021-04-22 10:04:56 -0300

    Terminado módulo JAMStack

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -39,9 +39,9 @@ export default function Post({ post }: PostProps) {
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req });
 
-  const { slug } = params;
+  const { slug } = params;  
 
-  if(!session.activeSubscription) {
+  if(!session?.activeSubscription) {
     return {
       redirect: {
         destination: '/',
